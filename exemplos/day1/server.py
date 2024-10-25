2d571bd2d3a3352104e8ca4a9fa8dbce367e61a6
vmagueta <solermvictor@gmail.com>
2024-10-25 16:53:01 -0300

    Finalizada aula de Server HTTP

---
+++ exemplos/day1/server.py
@@ -0,0 +1,20 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 9000))
+server.listen()
+
+try:
+    while True:
+        client, address = server.accept()
+        # Request
+        data = client.recv(5000).decode()
+        print(f"{data=}")
+
+        # Response
+        client.sendall(
+            "HTTP/1.0 200 OK\r\n\r\n<html><body>Hello</body></html>\r\n\r\n".encode()
+        )
+        client.shutdown(socket.SHUT_WR)
+except Exception:
+    server.close()
